89a6c7d6311c54b821df88236058b7377bedc82d
dsrini94 <sendtosrini@outlook.com>
2024-11-04 08:46:37 +0000

    Add efficient solution for max profit

--- JavaScript/src/max-profit-stocks/index.js
+++ JavaScript/src/max-profit-stocks/index.js
@@ -1,4 +1,12 @@
 export const maxProfitStocks = (input) => {
+  //spolution with o(n^2)
+  return soltionWith2ForLoop(input);
+
+  //efficient solution
+  return efficientSolution(input);
+};
+
+const soltionWith2ForLoop = (input) => {
   let maxProfit = 0;
   for (let i = 0; i < input.length; i++) {
     for (let j = i + 1; j < input.length; j++) {
@@ -6,5 +14,18 @@ export const maxProfitStocks = (input) => {
       if (currentProfit > maxProfit) maxProfit = currentProfit;
     }
   }
+  return maxProfit;
+};
+
+const efficientSolution = () => {
+  let maxProfit = 0;
+  let currentSellPrice = input[0];
+  for (let i = 1; i < input.length; i++) {
+    const currentProfit = input[i] - currentSellPrice;
+    maxProfit = Math.max(currentProfit, maxProfit);
+
+    if (input[i] < currentSellPrice) currentSellPrice = input[i];
+  }
+
   return maxProfit;
 };
